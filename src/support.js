e4d166f883c32be82859f1ab2fd75de387646e4e
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2019-02-06 12:27:48 +0000

    Revert

--- src/support.js
+++ src/support.js
@@ -5,8 +5,8 @@ module.exports = (function getSupport() {
         hasWheelEvent: 'onwheel' in document,
         hasMouseWheelEvent: 'onmousewheel' in document,
         hasTouch: 'ontouchstart' in document,
-        hasTouchWin: (navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1) || navigator.maxTouchPoints > 1,
-        hasPointer: (!!window.navigator.msPointerEnabled) || 'onpointerdown' in window,
+        hasTouchWin: (navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1)/* || navigator.maxTouchPoints > 1*/,
+        hasPointer: (!!window.navigator.msPointerEnabled)/* || 'onpointerdown' in window*/,
         hasKeyDown: 'onkeydown' in document,
         isFirefox: navigator.userAgent.indexOf('Firefox') > -1
     };
